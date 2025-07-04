061080a86c3315321fb1d8e91f34200aabeaa9d6
Anastasiia Hurska <nastyagurska@gmail.com>
2025-07-04 10:51:16 +0300

    (feat): add custom city to the storage

--- stores/cityStore.ts
+++ stores/cityStore.ts
@@ -1,5 +1,7 @@
 import { City } from "@/types";
-import { create } from "zustand";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface CitySelectorStore {
   selectorVisible: boolean;
@@ -8,9 +10,20 @@ interface CitySelectorStore {
   setCustomCity: (city: City | null) => void;
 }
 
-export const useCitySelectorStore = create<CitySelectorStore>((set) => ({
-  selectorVisible: false,
-  setSelectorVisible: (visible: boolean) => set({ selectorVisible: visible }),
-  customCity: null,
-  setCustomCity: (city: City | null) => set({ customCity: city }),
-}))
+export const useCitySelectorStore = create<CitySelectorStore>()(
+  persist(
+    (set) => ({
+      selectorVisible: false,
+      setSelectorVisible: (visible: boolean) => set({ selectorVisible: visible }),
+      customCity: null,
+      setCustomCity: (city: City | null) => set({ customCity: city }),
+    }),
+    {
+      name: 'city-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+      partialize: (state) => ({
+        customCity: state.customCity,
+      }),
+    }
+  )
+);
