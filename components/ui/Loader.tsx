48f6f2b869259357ec01462ee2eac6facff780ca
Anastasiia Hurska <nastyagurska@gmail.com>
2025-07-08 21:56:12 +0300

    (style): change Loader color to match app theme

--- components/ui/Loader.tsx
+++ components/ui/Loader.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
-import { View, ActivityIndicator } from 'react-native'
+import { ActivityIndicator, View } from 'react-native'
+import { useTheme } from 'react-native-paper'
 
 const Loader: React.FC = () => {
+  const theme = useTheme()
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
-      <ActivityIndicator animating={true} />
+      <ActivityIndicator animating={true} color={theme.colors.primary} />
     </View>
   )
 }
