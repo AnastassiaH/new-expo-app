529f72a446df8b0ef823ab6cb9b74637086156d6
Anastasiia Hurska <nastyagurska@gmail.com>
2025-07-10 16:27:44 +0300

    (fix): fix autologout timer behaviour

--- axios.ts
+++ axios.ts
@@ -1,6 +1,5 @@
 import { useAuthStore } from '@/stores/authStore';
 import axios from 'axios';
-import * as SecureStore from 'expo-secure-store';
 
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL
 
@@ -9,13 +8,6 @@ export const axiosService = axios.create({
 })
 
 axiosService.interceptors.request.use(async (config) => {
-  const url = config.url || '';
-
-  if (!url.includes('/partners')) {
-    await SecureStore.setItemAsync('lastActivityTime', Date.now().toString());
-    useAuthStore.getState().setLastActivityTime(Date.now());
-  }
-
   const token = useAuthStore.getState().session;
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
