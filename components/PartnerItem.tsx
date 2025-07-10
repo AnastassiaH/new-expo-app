be256ca84dba7673f80ecf11acbd8a36ec15909e
Anastasiia Hurska <nastyagurska@gmail.com>
2025-07-10 15:16:49 +0300

    (feractor): renew partner item

--- components/PartnerItem.tsx
+++ components/PartnerItem.tsx
@@ -1,52 +1,70 @@
 import { PartnerData } from '@/types';
 import React, { useRef } from 'react';
-import { Animated, Easing, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import {
+  Animated,
+  Easing,
+  LayoutAnimation,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  UIManager
+} from 'react-native';
 import { Card, useTheme } from 'react-native-paper';
-import PartnerActions from './feature/PartnerActions';
+import SocialBlock from './feature/SocialBlock';
 
 interface PartnerItemProps {
   item: PartnerData;
 }
 
+if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 const PartnerItem: React.FC<PartnerItemProps> = ({ item }) => {
-  const theme = useTheme()
-  const [expanded, setExpanded] = React.useState(false)
-  const fadeAnim = useRef(new Animated.Value(0)).current
+  const theme = useTheme();
+  const [expanded, setExpanded] = React.useState(false);
+  const fadeAnim = useRef(new Animated.Value(0)).current;
 
   const toggleExpanded = () => {
-    const toValue = expanded ? 0 : 1
-    setExpanded(!expanded)
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+
+    setExpanded(!expanded);
+
     Animated.timing(fadeAnim, {
-      toValue,
+      toValue: expanded ? 0 : 1,
       duration: 300,
       useNativeDriver: true,
       easing: Easing.inOut(Easing.ease),
-    }).start()
-  }
+    }).start();
+  };
 
   return (
-    <TouchableOpacity onPress={toggleExpanded}>
-      <Card mode='elevated' style={[styles.container, { backgroundColor: theme.colors.surface }]}>
-        <Card.Title title={item.placeFrom.name} />
-        <Card.Content>
-          <Text style={[styles.distanceText, { color: theme.colors.onSurfaceVariant }]}>{item.user?.firstName} within {item.placeFrom.distance} m</Text>
-        </Card.Content>
-        <Animated.View
-          style={{
-            opacity: fadeAnim,
-            transform: [{
-              translateY: fadeAnim.interpolate({
-                inputRange: [0, 1],
-                outputRange: [20, 0],
-              }),
-            }],
-          }}
-        >
-          <Card.Actions>
-            <PartnerActions item={item} />
-          </Card.Actions>
-        </Animated.View>
+    <TouchableOpacity onPress={toggleExpanded} activeOpacity={0.9}>
+      <Card
+        mode="elevated"
+        style={[styles.container, { backgroundColor: theme.colors.primary }]}
+      >
+        <Card.Title title={item.placeFrom.name} subtitle={`${item.user?.firstName} within ${item.placeFrom.distance} m`} subtitleVariant='bodyMedium' />
+
+        {expanded && (
+          <Animated.View
+            style={{
+              opacity: fadeAnim,
+              transform: [
+                {
+                  translateY: fadeAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [20, 0],
+                  }),
+                },
+              ],
+            }}
+          >
+            <Card.Actions>
+              <SocialBlock phone={item.user?.phoneNumber || ''} />
+            </Card.Actions>
+          </Animated.View>
+        )}
       </Card>
     </TouchableOpacity>
   );
@@ -60,24 +78,6 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 15,
     paddingVertical: 5,
-  },
-  placeName: {
-    fontSize: 16,
-    fontWeight: '500',
-  },
-  distanceContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 10,
-  },
-  distanceText: {
-    fontSize: 14,
-    color: '#666',
-  },
-  phoneText: {
-    marginTop: 10,
-    fontSize: 14,
-    color: '#333',
   },
 });
 
--- components/feature/SocialBlock.tsx
+++ components/feature/SocialBlock.tsx
@@ -1,7 +1,7 @@
-import { PartnerData } from '@/types'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import React from 'react'
 import { Alert, Linking, Text, TouchableOpacity, View } from 'react-native'
+import { useTheme } from 'react-native-paper'
 import TelegramIcon from '../icons/TelegramIcon'
 import ViberIcon from '../icons/ViberIcon'
 
@@ -23,22 +23,25 @@ export const handlePressApp = async (url: string, appName?: string) => {
   }
 };
 
-export default function PartnerActions({ item }: { item: PartnerData }) {
+export default function SocialBlock({ phone }: { phone: string }) {
+  const theme = useTheme()
 
   const handlePressPhone = () => {
-    Linking.openURL(`tel:${item?.user?.phoneNumber}`)
+    Linking.openURL(`tel:${phone}`)
       .catch(err => {
         console.error(err);
-        Alert.alert('Не вдалося відкрити номер.');
+        Alert.alert('Не вдалось відкрити номер.');
       });
   };
 
+  if (!phone) return null;
+
   return (
     <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center' }}>
-      <TouchableOpacity onPress={handlePressPhone}><Text>{item?.user?.phoneNumber}</Text></TouchableOpacity>
-      <TouchableOpacity onPress={() => handlePressApp(`whatsapp://send?phone=${item?.user?.phoneNumber}`)}><Ionicons name="logo-whatsapp" size={iconSize} color="black" /></TouchableOpacity>
-      <TouchableOpacity onPress={() => handlePressApp(`tg://resolve?phone=${item?.user?.phoneNumber}`)}><TelegramIcon color="black" size={iconSize} /></TouchableOpacity>
-      <TouchableOpacity onPress={() => handlePressApp(`viber://contact?number=%2B${item?.user?.phoneNumber}`)}><ViberIcon color="black" size={iconSize} /></TouchableOpacity>
+      <TouchableOpacity onPress={handlePressPhone}><Text style={{ color: theme.colors.onSurface }}>{phone}</Text></TouchableOpacity>
+      <TouchableOpacity onPress={() => handlePressApp(`whatsapp://send?phone=${phone}`)}><Ionicons name="logo-whatsapp" size={iconSize} color={theme.colors.onSurface} /></TouchableOpacity>
+      <TouchableOpacity onPress={() => handlePressApp(`tg://resolve?phone=${phone}`)}><TelegramIcon color={theme.colors.onSurface} size={iconSize} /></TouchableOpacity>
+      <TouchableOpacity onPress={() => handlePressApp(`viber://contact?number=%2B${phone}`)}><ViberIcon color={theme.colors.onSurface} size={iconSize} /></TouchableOpacity>
     </View>
   )
 }
